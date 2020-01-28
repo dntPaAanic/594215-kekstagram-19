822de2ef822c34f3d6f0391c64de7d9c467bde44
Vitaly Mes <volkondr@ya.ru>
2020-01-28 11:47:02 +0300

    Исправления

--- js/main.js
+++ js/main.js
@@ -3,8 +3,32 @@
 var PICTURE_COUNT = 25;
 var LIKE_COUNT_MIN = 15;
 var LIKE_COUNT_MAX = 200;
-var AUTHOR_COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-var COMMENT_AUTHOR_NAMES = ['Jozef Contreras', 'Mohammed Robson', 'Gabriela Jefferson', 'Wyatt Alcock', 'Arjan Clarke', 'Dionne Burgess', 'Dewi Pace', 'Mujtaba Yates', 'Donnell Yoder', 'Axel Vega', 'Khaleesi Sargent', 'Lord James Sweeney', 'Frank Wade', 'Robert Santiago'];
+var AVATAR_MIN = 1;
+var AVATAR_MAX = 6;
+var AUTHOR_COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+var COMMENT_AUTHOR_NAMES = [
+  'Jozef Contreras',
+  'Mohammed Robson',
+  'Gabriela Jefferson',
+  'Wyatt Alcock',
+  'Arjan Clarke',
+  'Dionne Burgess',
+  'Dewi Pace',
+  'Mujtaba Yates',
+  'Donnell Yoder',
+  'Axel Vega',
+  'Khaleesi Sargent',
+  'Lord James Sweeney',
+  'Frank Wade',
+  'Robert Santiago'
+];
 
 var templatePictureItem = document.querySelector('#picture').content.querySelector('.picture');
 var pictureList = document.querySelector('.pictures');
@@ -19,12 +43,12 @@ var getRandomArrElement = function (arr) {
 };
 
 var getRandomComment = function () {
-  var Сomment = {
-    avatar: 'img/avatar-' + getRandomNumber(1, 6) + '.svg',
+  var comment = {
+    avatar: 'img/avatar-' + getRandomNumber(AVATAR_MIN, AVATAR_MAX) + '.svg',
     message: getRandomArrElement(AUTHOR_COMMENTS),
     name: getRandomArrElement(COMMENT_AUTHOR_NAMES),
   };
-  return Сomment;
+  return comment;
 };
 
 var getCommentsList = function (commentsCount) {
@@ -35,24 +59,15 @@ var getCommentsList = function (commentsCount) {
   return comments;
 };
 
-var getRandomPictureItem = function (imgUrl, description, likesCount, comment) {
-  var Photo = {
-    url: imgUrl,
-    description: description,
-    likes: likesCount,
-    comments: comment,
-  };
-  return Photo;
-};
-
 var getPictureList = function (pictureCount) {
   var arr = [];
   for (var i = 1; i <= pictureCount; i++) {
-    var pictureUrl = 'photos/' + i + '.jpg';
-    var pictureDescription = 'Описание фотографии - ' + i;
-    var pictureLikesCount = getRandomNumber(LIKE_COUNT_MIN, LIKE_COUNT_MAX);
-    var pictureComments = getCommentsList(getRandomNumber(1, 2));
-    arr.push(getRandomPictureItem(pictureUrl, pictureDescription, pictureLikesCount, pictureComments));
+    arr.push({
+      url: 'photos/' + i + '.jpg',
+      description: 'Описание фотографии - ' + i,
+      likes: getRandomNumber(LIKE_COUNT_MIN, LIKE_COUNT_MAX),
+      comments: getCommentsList(getRandomNumber(1, 2))
+    });
   }
   return arr;
 };
